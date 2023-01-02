0090302b9c1895b51fabfe272a470c0b83b63842
DeedleFake <deedlefake@users.noreply.github.com>
2023-01-01 23:40:06 -0500

    Initial Commit.

---
+++ android/app/src/main/kotlin/com/example/siv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.siv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as image;
+import 'package:http/http.dart' as http;
+import 'package:wakelock/wakelock.dart';
+
+void main(List<String> args) {
+  runApp(const Siv());
+}
+
+class Siv extends StatelessWidget {
+  static const title = 'Siv';
+
+  const Siv({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: (settings) => MaterialPageRoute(
+        builder: (context) {
+          return Home(uri: Uri.file('/'));
+        },
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  final Uri uri;
+
+  const Home({super.key, required this.uri});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<image.Image> _image;
+
+  Future<image.Image> _loadImage() async {
+    final rsp = await http.get(Uri.parse(
+        'https://chat.openai.com/_next/image?url=https%3A%2F%2Fs.gravatar.com%2Favatar%2F4eef6ac4f1e3d53b75375bc4509a3ba1%3Fs%3D480%26r%3Dpg%26d%3Dhttps%253A%252F%252Fcdn.auth0.com%252Favatars%252Fyi.png&w=32&q=75'));
+    return image.decodeImage(rsp.bodyBytes) ?? image.Image.empty();
+  }
+
+  Future<void> _enableWakelock() async {
+    try {
+      await Wakelock.enable();
+    } on PlatformException {
+      if (kDebugMode) {
+        print("Wakelock not supported on this platform.");
+      }
+    }
+  }
+
+  Future<void> _disableWakelock() async {
+    if (await Wakelock.enabled) {
+      Wakelock.disable();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _image = _loadImage();
+    _enableWakelock();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _disableWakelock();
+  }
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: FutureBuilder(
+          future: _image,
+          builder: (context, snapshot) {
+            var imageData = snapshot.data?.toUint8List();
+            return snapshot.hasData && (imageData != null)
+                ? Image.memory(imageData)
+                : const CircularProgressIndicator();
+          },
+        ),
+      );
+}
